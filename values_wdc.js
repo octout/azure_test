16d51e31dc783eefd7fa5d73abd7b2cc5622e786
octout <octout@users.noreply.github.com>
2020-06-20 03:03:44 +0900

    Update values_wdc.js

--- values_wdc.js
+++ values_wdc.js
@@ -1,4 +1,3 @@
-var tableData = [];
 (function () {
     var myConnector = tableau.makeConnector();
     var apiString = $('#apikey').val();
@@ -59,6 +58,7 @@ var tableData = [];
     };
 
     myConnector.getData = function(table, doneCallback) {
+        var tableData = [];
         $.getJSON("Obs.json", function(json) { //ローカルのjsonへアクセス
             console.log(json); // this will show the info it in firebug console
             console.log(json.length); // this will show the info it in firebug console
