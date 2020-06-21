953ae16c03e9285225d730edcd1b9d4a2dfa21ce
octout <octout@users.noreply.github.com>
2020-06-21 15:24:37 +0900

    Update values_wdc.js

--- values_wdc.js
+++ values_wdc.js
@@ -61,10 +61,11 @@
         var args = JSON.parse(tableau.connectionData),
             str_apikey = args.apikey,
             tableData = [];
+        var count = 5;
         $.getJSON("Obs.json", function(json) { //ローカルのjsonへアクセス
             for (var i = 0, len = json.length; i < len; i++) {
                 console.log("SuccessRead-json"+ " count:" + count);
-                var count = json.length;
+                //var count = json.length;
                 var dateString = "query=" + json[i].lat + "," + json[i].lon,
                     apiCall = "https://atlas.microsoft.com/weather/forecast/hourly/json?subscription-key=" + str_apikey +"&api-version=1.0&" + dateString + "&duration=72&language=ja";
                     (function(t){
